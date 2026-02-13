53f5edbc4038fc10e5ebf25b0f81941a2068a55c
colonelvoid <parthnaik.office@gmail.com>
2026-02-13 23:35:58 +0530

    promises

---
+++ bootdev/promise.js
@@ -0,0 +1,22 @@
+// promises are used to handle async code instead of callbacks
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (getRandomBool())
+      resolve("resolved");
+    else
+      reject("rejected");
+  }, 1000);
+});
+
+function getRandomBool() {
+  return Math.random() < 0.5
+}
+
+promise
+  .then((message) => {
+    console.log(`The promise finally ${message}`);
+  })
+  .catch((message) => {
+    console.log(`The promise finally ${message}`);
+  });
