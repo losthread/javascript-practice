ca7c65e697df0d900bb591e2316cd273387feba9
f8tum <parthnaik.office@gmail.com>
2026-03-31 15:53:05 +0530

    import and export

---
+++ react/first-vite-app/src/App.jsx
@@ -0,0 +1,7 @@
+import Gallery from './Component'
+
+export default function App() {
+  return (
+    <Gallery />
+  );
+}
--- react/first-vite-app/src/main.jsx
+++ react/first-vite-app/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Gallery from './Component'
+import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* components can be used like custon html tags */}
-    <Gallery />
+    <App/>
   </StrictMode>,
 )
