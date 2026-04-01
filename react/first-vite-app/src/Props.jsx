bbe688f075350e121ad14a82c75db86f632b0470
f8tum <parthnaik.office@gmail.com>
2026-04-01 09:06:32 +0530

    jsx props and tailwind

---
+++ react/first-vite-app/src/Props.jsx
@@ -0,0 +1,44 @@
+// Components use props to communicate with each other
+// Every parent component can pass some info to its child components with props
+import { getImageUrl } from './utils.js';
+
+// unpacking the props
+function Avatar({person, size}) { 
+  return (
+    <img
+      className='avatar rounded-[50%]'
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+export default function Profile() {
+  return (
+    <div className='flex gap-4 justify-center'>
+      <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi', 
+          imageId: 'YfeOqp2'
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: 'Aklilu Lemma', 
+          imageId: 'OKS67lh'
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{ 
+          name: 'Lin Lanying',
+          imageId: '1bX5QH6'
+        }}
+      />
+    </div>
+  );
+}
--- react/first-vite-app/src/main.jsx
+++ react/first-vite-app/src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Gallery from './Component'
 import App from './App'
 import './index.css'
+import Gallery from './Component'
 import Avatar from './Avatar'
+import Profile from './Props'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* components can be used like custon html tags */}
     <App/>
-    <Avatar/>
+    <Profile />
   </StrictMode>,
 )
